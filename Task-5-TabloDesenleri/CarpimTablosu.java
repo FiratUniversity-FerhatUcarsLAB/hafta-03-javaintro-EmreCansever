e01380befc5f40955c39b31c34351b90b4593aa6
EmreCansever <250541060@firat.edu.tr>
2025-10-22 16:20:11 +0300

    CarpimTablosu.java

--- Task-5-TabloDesenleri/CarpimTablosu.java
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -1,30 +1,28 @@
 public class Main {
       public static void main (String[] args){
-        int a1= 1;
-        int a2= a1*a1;
-        int a3= a2*a1;
-        int b1= 2;
-        int b2= b1*b1;
-        int b3= b2*b1;
-        int c1= 3;
-        int c2= c1*c1;
-        int c3= c2*c1;
-        int d1= 4;
-        int d2= d1*d1;
-        int d3= d2*d1;
-        int e1= 5;
-        int e2= e1*e1;
-        int e3= e2*e1;
+        int a1= 2;
+        int a2= 2*2;
+        int a3= 2*3;
+        int a4= 2*4;
+        int a5= 2*5;
+        int a6= 2*6;
+        int a7= 2*7;
+        int a8= 2*8;
+        int a9= 2*9;
+        int a10= 2*10;
 
-
-          System.out.println("a   a^2   a^3");
-          System.out.println("-------------");
-          System.out.println(a1 + "    " + a2 + "    " + a3);
-          System.out.println(b1 + "    " + b2 + "    " + b3);
-          System.out.println(c1 + "    " + c2 + "    " + c3);
-          System.out.println(d1 + "    " + d2 + "    " + d3);
-          System.out.println(e1 + "    " + e2 + "    " + e3);
-
+          System.out.println("2x1 = " + a1);
+          System.out.println("2x2 = " + a2);
+          System.out.println("2x3 = " + a3);
+          System.out.println("2x4 = " + a4);
+          System.out.println("2x5 = " + a5);
+          System.out.println("2x6 = " + a6);
+          System.out.println("2x7 = " + a7);
+          System.out.println("2x8 = " + a8);
+          System.out.println("2x9 = " + a9);
+          System.out.println("2x10 = " + a10);
       }
 
 }
+}
+
