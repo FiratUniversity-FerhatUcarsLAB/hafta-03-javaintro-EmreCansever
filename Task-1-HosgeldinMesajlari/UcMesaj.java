a51fdf36c2ab22ad60620f3ccc86d1997b53d9e8
EmreCansever <250541060@firat.edu.tr>
2025-10-22 14:09:53 +0300

    UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,9 @@
+public class Main {
+      public static void main (String[] args){
+          System.out.println("Java'ya Hos geldiniz");
+          System.out.println("Bilgisayar Bilimine Hos Geldiniz");
+          System.out.println("Programlama Eglencelidir");
+
+      }
+
+}
