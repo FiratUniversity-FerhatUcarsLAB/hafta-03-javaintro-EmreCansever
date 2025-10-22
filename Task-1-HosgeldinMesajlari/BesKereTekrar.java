b921054e8e2cd7a27b72b5eaf43f41be14b06076
EmreCansever <250541060@firat.edu.tr>
2025-10-22 14:11:35 +0300

    BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,11 @@
+public class Main {
+      public static void main (String[] args){
+          System.out.println("Java'ya Hos geldiniz");
+          System.out.println("Java'ya Hos geldiniz");
+          System.out.println("Java'ya Hos geldiniz");
+          System.out.println("Java'ya Hos geldiniz");
+          System.out.println("Java'ya Hos geldiniz");
+
+      }
+
+}
