46e1ca03198e604b1e29a00cfcd9c8489c65a055
EmreCansever <250541060@firat.edu.tr>
2025-10-22 14:42:29 +0300

    DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,15 @@
+public class Main {
+      public static void main (String[] args){
+        double en= 4.5;
+        double boy= 7.9;
+
+        double alan= en*boy;
+        double cevre= (en+boy)*2;
+
+          System.out.println("alan = " + alan);
+          System.out.println("cevre = " + cevre);
+
+
+      }
+
+}
