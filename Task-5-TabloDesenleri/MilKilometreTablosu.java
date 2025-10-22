1ff0133d68b5fe6f8c5594ce94d69ab5812ca7fb
EmreCansever <250541060@firat.edu.tr>
2025-10-22 16:37:17 +0300

    MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,23 @@
+public class Main {
+      public static void main (String[] args){
+
+        int km1 = 1;
+        int km2 = 2;
+        int km3 = 3;
+        int km4 = 4;
+        int km5 = 5;
+        int mil1= 1609*km1;
+        int mil2= 1609*km2;
+        int mil3= 1609*km3;
+        int mil4= 1609*km4;
+        int mil5= 1609*km5;
+
+          System.out.println("1 Mil = " + mil1);
+          System.out.println("2 Mil = " + mil2);
+          System.out.println("3 Mil = " + mil3);
+          System.out.println("4 Mil = " + mil4);
+          System.out.println("5 Mil = " + mil5);
+
+      }
+
+}
