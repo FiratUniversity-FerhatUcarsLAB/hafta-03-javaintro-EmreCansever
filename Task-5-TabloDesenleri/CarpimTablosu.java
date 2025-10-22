4ecf177f304d408f741e0d10794feb5cfd7b06b5
EmreCansever <250541060@firat.edu.tr>
2025-10-22 16:12:42 +0300

    CarpimTablosu.java
    Prints a table of numbers with their squares and cubes.

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,30 @@
+public class Main {
+      public static void main (String[] args){
+        int a1= 1;
+        int a2= a1*a1;
+        int a3= a2*a1;
+        int b1= 2;
+        int b2= b1*b1;
+        int b3= b2*b1;
+        int c1= 3;
+        int c2= c1*c1;
+        int c3= c2*c1;
+        int d1= 4;
+        int d2= d1*d1;
+        int d3= d2*d1;
+        int e1= 5;
+        int e2= e1*e1;
+        int e3= e2*e1;
+
+
+          System.out.println("a   a^2   a^3");
+          System.out.println("-------------");
+          System.out.println(a1 + "    " + a2 + "    " + a3);
+          System.out.println(b1 + "    " + b2 + "    " + b3);
+          System.out.println(c1 + "    " + c2 + "    " + c3);
+          System.out.println(d1 + "    " + d2 + "    " + d3);
+          System.out.println(e1 + "    " + e2 + "    " + e3);
+
+      }
+
+}
