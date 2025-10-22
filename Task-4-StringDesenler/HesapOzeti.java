d85c387288ed676041f50feb0956f5bb56506481
EmreCansever <250541060@firat.edu.tr>
2025-10-22 15:53:16 +0300

    HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,25 @@
+public class Main {
+      public static void main (String[] args){
+
+          int uzummıktar= 6;
+          int elmamıktar= 8;
+          int karpuzmıktar= 2;
+          int uzum= uzummıktar*13;
+          int elma= elmamıktar*7;
+          int karpuz= karpuzmıktar*3;
+
+
+
+
+
+          System.out.println("---------------------");
+          System.out.println("    HESAP OZETI      ");
+          System.out.println("---------------------");
+          System.out.println("              MIKTAR          FIYAT");
+          System.out.println("Uzum =" + "          " + uzummıktar + "              " + uzum);
+          System.out.println("Elma =" + "          " + elmamıktar + "              " + elma);
+          System.out.println("Karpuz =" + "        " + karpuzmıktar + "               " + karpuz);
+
+      }
+
+}
