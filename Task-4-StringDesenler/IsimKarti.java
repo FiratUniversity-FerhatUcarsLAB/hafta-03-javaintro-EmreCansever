427fb2fc102d50e95db8a8bc172496576aff2e81
EmreCansever <250541060@firat.edu.tr>
2025-10-22 15:25:25 +0300

    IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,19 @@
+public class Main {
+      public static void main (String[] args){
+          String ad= ("Emre");
+          String soyad= ("Cansever");
+          String Ogrno= ("250541060");
+          String bolum= ("Yazılım Muh.");
+
+          System.out.println("-------------");
+          System.out.println("OGRENCI BILGILERI");
+          System.out.println("-------------");
+          System.out.println("Ad = " + ad);
+          System.out.println("Soyad = " + soyad);
+          System.out.println("No = " + Ogrno);
+          System.out.println("Bolum = " + bolum);
+          System.out.println("--------------");
+
+      }
+
+}
